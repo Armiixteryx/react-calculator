a72c1727cb204e75990fbd78e4463cf48c398fca
David Camperos <davidapks@gmail.com>
2020-04-21 20:37:52 -0400

    Added title and display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { Header } from '@fluentui/react-northstar';
+
+import Display from './components/Display';
+
+const styles = {
+  width: '500px',
+  margin: '0 auto',
+}
+
 function App() {
   return (
-    <div className="App">
-      <p>
-        Hello world!
-      </p>
+    <div style={styles}>
+      <Header align='center' content='SIMPLE CALCULATOR' />
+      <Display textDisplay="Display" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider, themes } from '@fluentui/react-northstar';
+//import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider theme={themes.teams} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
